ce9e410e08c7891af6b5a9cc3e37af42bca74422
Saurabhh <sjoshi6@ncsu.edu>
2015-03-24 22:25:12 -0400

    apps data

--- nodejs_server_setup/requests.js
+++ nodejs_server_setup/requests.js
@@ -23,9 +23,27 @@ exports.insert_app_data_in_db = function(data,callback) {
       var apps = data.app;
 
       apps.forEach(function(data){
-          console.log(data);
+          var id = data.id;
+          var query = connection.query("SELECT * from application_data where id='"+id+"';", function(err, rows)
+                                {
+                                if(rows.length == 0){
+                                  var query = connection.query("INSERT INTO application_data set ? ",data, function(err, rows)
+                                               {
+                                                 callback({'response':"Sucessfully added"});
+                                               });
+
+                                }else
+                                {
+                        //          var query = connection.query("UPDATE application_data set ? ",data, function(err, rows)
+                        //                     {
+                        //                     callback({'response':"Sucessfully added"});
+                        //                     });
+                                }
+
+                                });
+}
       });
-	
+
 	callback({'response':'Added Apps'})
       //var query = connection.query("INSERT INTO node_data set ? ",data, function(err, rows)
       //                              {
