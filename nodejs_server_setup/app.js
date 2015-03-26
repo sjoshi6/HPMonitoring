646dbc69f55ca71ce7de8721e4cc0cd3e9f00c2a
Saurabhh <sjoshi6@ncsu.edu>
2015-03-26 17:13:30 -0400

    Open app

--- nodejs_server_setup/app.js
+++ nodejs_server_setup/app.js
@@ -11,5 +11,11 @@ app.use(connect.urlencoded());
 // Routes
 var routedata = require('./routes.js')
 routedata(app);
-app.listen(port);
-console.log('The App runs on port ' + port);
+
+app.listen(port, function () {
+
+	  var host = server.address().address
+	  var port = server.address().port
+
+   	console.log('Example app listening at http://%s:%s', host, port)
+});
