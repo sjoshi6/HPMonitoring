c9f8c9d489e27db65a216f356bd218689c757c53
Saurabhh <sjoshi6@ncsu.edu>
2015-03-26 20:21:35 -0400

    HeartBeat Sender iostat

---
+++ heartbeat_senders/iostat_heartbeatsender.sh
@@ -0,0 +1,7 @@
+while [ true ]
+do
+        sleep 30
+	iostat -x > iostat_data
+	python /iostat_to_json.py iostat_data
+        curl -d @iostat_dataout.json --header "Content-Type:application/json" 152.46.20.151:8080/iostatdata
+done
