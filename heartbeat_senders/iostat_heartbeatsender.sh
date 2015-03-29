795d58a8722f55de63af205208ef4dfab5bbc803
Saurabhh <sjoshi6@ncsu.edu>
2015-03-29 16:48:46 -0400

    edited IP Address

--- heartbeat_senders/iostat_heartbeatsender.sh
+++ heartbeat_senders/iostat_heartbeatsender.sh
@@ -3,5 +3,5 @@ do
         sleep 30
 	iostat -x > iostat_data
 	python /iostat_to_json.py iostat_data
-        curl -d @iostat_dataout.json --header "Content-Type:application/json" 152.46.20.151:8080/iostatdata
+        curl -d @iostat_dataout.json --header "Content-Type:application/json" 152.46.19.52:8080/iostatdata
 done
