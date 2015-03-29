a5fccd6f5579c46f57cd5ffa7235931fadda684d
Saurabhh <sjoshi6@ncsu.edu>
2015-03-29 19:54:40 -0400

    >> to >

--- heartbeat_senders/node_data_heartbeatsender.sh
+++ heartbeat_senders/node_data_heartbeatsender.sh
@@ -4,6 +4,6 @@
 while [ true ]
 do
   sleep 10
-  curl -H "Content-Type: application/json" http://152.46.20.151:8088/ws/v1/cluster/nodes/$1 >> $2.json
+  curl -H "Content-Type: application/json" http://152.46.20.151:8088/ws/v1/cluster/nodes/$1 > $2.json
 	curl -d @$2.json --header "Content-Type:application/json" 152.46.19.52:8080/nodedata
 done
