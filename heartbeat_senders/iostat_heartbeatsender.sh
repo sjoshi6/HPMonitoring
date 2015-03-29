511696f2ff4833f6f12950b880875208ba153093
Saurabhh <sjoshi6@ncsu.edu>
2015-03-29 17:56:41 -0400

    file path change

--- heartbeat_senders/iostat_heartbeatsender.sh
+++ heartbeat_senders/iostat_heartbeatsender.sh
@@ -2,6 +2,6 @@ while [ true ]
 do
         sleep 30
 	iostat -x > iostat_data
-	python /iostat_to_json.py iostat_data
+	python ../python_scripts/iostat_to_json.py iostat_data
         curl -d @iostat_dataout.json --header "Content-Type:application/json" 152.46.19.52:8080/iostatdata
 done
