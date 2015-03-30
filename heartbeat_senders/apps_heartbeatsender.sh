e4be8c7bdbeeae46a09edda56dc79c34f8bcda99
Saurabhh <sjoshi6@ncsu.edu>
2015-03-29 21:11:25 -0400

    Apps Data Collector

---
+++ heartbeat_senders/apps_heartbeatsender.sh
@@ -0,0 +1,9 @@
+
+#Usage apps_data_hearbeatsender
+
+while [ true ]
+do
+  sleep 10
+  curl -H "Content-Type: application/json" http://152.46.20.151:8088/ws/v1/cluster/apps > apps_data.json
+	curl -d @apps_data.json --header "Content-Type:application/json" 152.46.19.52:8080/appsdata
+done
