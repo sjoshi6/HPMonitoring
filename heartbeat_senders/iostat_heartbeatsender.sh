f1e339ff4acf868c2c89d34e090284374785ebcd
Saurabhh <sjoshi6@ncsu.edu>
2015-04-15 11:12:00 -0400

    heartbeat senders

--- heartbeat_senders/iostat_heartbeatsender.sh
+++ heartbeat_senders/iostat_heartbeatsender.sh
@@ -1,7 +1,9 @@
+#Usage: ./iostat_heartbeatsender.sh IPAddress_of_node
+
 while [ true ]
 do
-        sleep 30
+  sleep 5
 	iostat -x 1 2 > iostat_data
-	python ../python_scripts/iostat_to_json.py iostat_data
-        curl -d @iostat_dataout.json --header "Content-Type:application/json" 152.46.19.52:8080/iostatdata
+	python ../python_scripts/iostat_to_json.py iostat_data $1
+  curl -d @iostat_dataout.json --header "Content-Type:application/json" 152.46.19.52:8080/iostatdata
 done
